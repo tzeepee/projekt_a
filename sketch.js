ef7986d1048112070567fd0a045074cb476a2025
tzeepee <cpannacci@techfak.uni-bielefeld.de>
2024-02-19 13:53:24 +0100

    erster_versuch

--- sketch.js
+++ sketch.js
@@ -1,10 +1,54 @@
+let table;
+
+function preload() {
+  table = loadTable('schau_fil_beziehung.csv', 'csv', 'header');
+}
+
+let data = [];
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-    background(255,0,0);
+  for (let row of table.rows) {
+    let actor_id = row.get('schauspieler_id');
+    let actor_name = row.get('schauspieler_name');
+    let movie_id = row.get('film_id');
+    let movie_name = row.get('film_name');
+    data.push({actor_id, actor_name, movie_id, movie_name});
   }
+}
+
+let actors = {};
+let movies = {};
+
+function setup() {
+  createCanvas(800, 800);
+  for (let row of table.rows) {
+    let actor_id = row.get('schauspieler_id');
+    let actor_name = row.get('schauspieler_name');
+    let movie_id = row.get('film_id');
+    let movie_name = row.get('film_name');
+    // Erzeugen Sie zufällige x- und y-Koordinaten für jeden Knoten
+    let x = random(width);
+    let y = random(height);
+    // Speichern Sie die Positionen der Schauspieler und Filme
+    if (!actors[actor_id]) {
+      actors[actor_id] = {x, y};
+    }
+    if (!movies[movie_id]) {
+      movies[movie_id] = {x, y};
+    }
+    data.push({actor_id, actor_name, movie_id, movie_name});
+  }
+}
 
 function draw() {
-  ellipse(100, 100, 80, 40);
+  background(220);
+  for (let item of data) {
+    let actor_id = item.actor_id;
+    let movie_id = item.movie_id;
+    // Zeichnen Sie die Knoten an den gespeicherten Positionen
+    ellipse(actors[actor_id].x, actors[actor_id].y, 20, 20);
+    ellipse(movies[movie_id].x, movies[movie_id].y, 20, 20);
+    // Zeichnen Sie eine Linie zwischen den Schauspieler- und Filmknoten
+    line(actors[actor_id].x, actors[actor_id].y, movies[movie_id].x, movies[movie_id].y);
   }
-
-  
+}
